03e668b0ddff3ba91ef44ed4118a707001574833
Warren Hansen <whansen1@me.com>
2018-07-08 14:10:59 -0500

    task: added main vc

--- Shake Animation/ViewController.swift
+++ Shake Animation/ViewController.swift
@@ -9,10 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBOutlet weak var cardView: UIView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
